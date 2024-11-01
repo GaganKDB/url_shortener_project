ac3b3becd8848b8901efbc20fa3730a3a589c818
GAGAN PRATAP SINGH <21bcs053@nith.ac.in>
2024-11-01 16:58:31 +0530

    Next version comments added

--- connect.js
+++ connect.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", true);
 async function connectToMongoDB(url) {
     return mongoose.connect(url);
 }
-
+// exporting the schema as a model here and some modify changes with comment 
 module.exports = {
     connectToMongoDB,
 };
